c80c32693ca55ff09b4ee85ad9bbce74badde48f
Wilhelm Flores <wiljoseflores@gmail.com>
2024-04-02 19:03:52 -0400

    feat: base enviroment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from '@/components/common/Header'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className='flex flex-col bg-gray-400' >
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
